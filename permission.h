9a74a1920a7a95d312bd3de7bacd10f49ea1d136
SimpleSoft <119447551+VolderProgrramer20@users.noreply.github.com>
2024-05-31 14:44:38 +0300

    Add files via upload

---
+++ permission.h
@@ -0,0 +1,13 @@
+enum permission
+{
+    read_file,
+    write_folder,
+    write_file,
+    delete_folder,
+    delete_file,
+    delete_user,
+    rename_user,
+    add_permisson,
+    delete_permission,
+    root
+};
